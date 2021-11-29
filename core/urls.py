3144f83f5425d76590883194e806d84d8e560244
flavien-hugs <flavienhugs@pm.me>
2021-11-29 11:05:04 +0000

    update settings parameters and urls

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ core URL Configuration
 
 from django.contrib import admin
 from django.conf import settings
+from django.views import generic
 from django.shortcuts import render
 from django.urls import path, include
 from django.contrib.auth import views
@@ -15,12 +16,20 @@ admin.site.index_title = f"WELCOME TO {admin.site.site_header}"
 
 
 urlpatterns = [
-    path(route='', view=views.LoginView.as_view(
+    path(route='', view=generic.TemplateView.as_view(
+        template_name="board.html",
+        extra_context={"page_title": "dashboard"}
+    ), name='dashboard'),
+
+    path(route='login/', view=views.LoginView.as_view(
         template_name="login.html",
         extra_context={"page_title": "connexion"}
     ), name='login'),
     path(route='deconnexion/', view=views.LogoutView.as_view(), name='logout'),
-    path("d/", include('visitors.urls', namespace="visitors")),
+
+    path('flex/', include('flex.urls', namespace='flex')),
+    path('max/', include('max.urls', namespace='max')),
+    
     path('jet/', include('jet.urls', 'jet')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path(settings.ADMIN_URL, admin.site.urls),
